d45b17966a56d85c1ae86da3a4ea239e00df80ca
Lyusi Divakov <divakovwork@gmail.com>
2023-11-09 22:18:10 +0200

    SMALL chnags in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,10 +4,10 @@ import { ArrowRightIcon } from "@heroicons/react/24/outline";
 
 export const Footer = () => {
   return (
-    <footer className="mx-auto mt-32 w-full pb-8 max-w-container px-4 sm:px-6 lg:px-8 bg-gray-800">
+    <footer className="mx-auto mt-32 w-full pb-8 max-w-container px-4 sm:px-6 lg:px-32 bg-gray-800">
       <div className="grid pt-16 pb-8 ">
-        <ul className="grid grid-cols-3 text-white text-left">
-          <li className="w-40">
+        <ul className="grid sm:grid-cols-2 text-center sm:text-left md:grid-cols-4 text-white text-left">
+          <li className="grid py-10">
             <h3 className="font-medium decoration-solid underline underline-offset-2 text-lg">Shop</h3>
             <div className="grid grid-cols-1 gap-y-3 mt-4 text-sm">
               <a href="#">Women</a>
@@ -17,7 +17,7 @@ export const Footer = () => {
               <a href="#">Accessories</a>
             </div>
           </li>
-          <li className="w-40">
+          <li className="grid py-10">
             <h3 className="font-medium decoration-solid underline underline-offset-2 text-lg">Help</h3>
             <div className="grid grid-cols-1 gap-y-3 mt-4 text-sm">
               <a href="#">Contact Us</a>
@@ -27,16 +27,26 @@ export const Footer = () => {
               <a href="#">Cookie Settings</a>
             </div>
           </li>
-          <li className="w-56">
+          <li className="grid py-10">
+          <h3 className="font-medium decoration-solid underline underline-offset-2 text-lg">Social Media</h3>
+            <div className="grid grid-cols-1 gap-y-3 mt-4 text-sm">
+              <a href="#">Facebook</a>
+              <a href="#">Instagram</a>
+              <a href="#">Pinterest</a>
+              <a href="#">Youtube</a>
+              <a href="#">Tik Tok</a>
+            </div>
+          </li>
+          <li className="grid py-10">
             <h3 className="font-medium decoration-solid underline underline-offset-2 text-lg">Join us Now</h3>
             <div className="grid grid-cols-1 gap-y-5 mt-4 text-sm">
               <p>Becom a member today and get 10% off your first purchase</p>
-              <a className="uppercase flex decoration-solid hover:underline underline-offset-2" href="#">Read more <ArrowRightIcon className=" ml-2 h-5 w-5" aria-hidden="true" /></a>
+              <a className="uppercase flex decoration-solid hover:underline underline-offset-2 mx-auto" href="#">Read more <ArrowRightIcon className="ml-2 h-5 w-5" aria-hidden="true" /></a>
             </div>
           </li>
         </ul>
       </div>
-      <div className="py-8">
+      <div className="py-2 sm:py-8">
         <svg
           className="mx-auto h-5 w-auto text-white"
           aria-hidden="true"
@@ -46,7 +56,7 @@ export const Footer = () => {
           <path
             d="M18.724 1.714c-4.538 0-7.376 2.286-8.51 6.857 1.702-2.285 3.687-3.143 5.957-2.57 1.296.325 2.22 1.271 3.245 2.318 1.668 1.706 3.6 3.681 7.819 3.681 4.539 0 7.376-2.286 8.51-6.857-1.701 2.286-3.687 3.143-5.957 2.571-1.294-.325-2.22-1.272-3.245-2.32-1.668-1.705-3.6-3.68-7.819-3.68zM10.214 12c-4.539 0-7.376 2.286-8.51 6.857 1.701-2.286 3.687-3.143 5.957-2.571 1.294.325 2.22 1.272 3.245 2.32 1.668 1.705 3.6 3.68 7.818 3.68 4.54 0 7.377-2.286 8.511-6.857-1.702 2.286-3.688 3.143-5.957 2.571-1.295-.326-2.22-1.272-3.245-2.32-1.669-1.705-3.6-3.68-7.82-3.68z"
             className="fill-sky-400"
-          ></path>
+          ></path> 
           <path
             d="M51.285 9.531V6.857h-3.166v-3.6l-2.758.823v2.777h-2.348v2.674h2.348v6.172c0 3.343 1.686 4.526 5.924 4.011V17.22c-2.094.103-3.166.129-3.166-1.517V9.53h3.166zm12.087-2.674v1.826c-.97-1.337-2.476-2.16-4.468-2.16-3.472 0-6.357 2.931-6.357 6.763 0 3.805 2.885 6.763 6.357 6.763 1.992 0 3.498-.823 4.468-2.186v1.851h2.758V6.857h-2.758zM59.338 17.4c-2.297 0-4.034-1.723-4.034-4.114 0-2.392 1.736-4.115 4.034-4.115s4.034 1.723 4.034 4.115c0 2.391-1.736 4.114-4.034 4.114zM70.723 4.929c.97 0 1.762-.823 1.762-1.775 0-.977-.792-1.774-1.762-1.774s-1.762.797-1.762 1.774c0 .952.792 1.775 1.762 1.775zm-1.379 14.785h2.758V6.857h-2.758v12.857zm5.96 0h2.757V.943h-2.758v18.771zM95.969 6.857l-2.502 8.872-2.655-8.872h-2.63L85.5 15.73l-2.477-8.872h-2.91l4.008 12.857h2.707l2.68-8.665 2.656 8.665h2.706L98.88 6.857h-2.911zm6.32-1.928c.97 0 1.762-.823 1.762-1.775 0-.977-.792-1.774-1.762-1.774s-1.762.797-1.762 1.774c0 .952.792 1.775 1.762 1.775zm-1.379 14.785h2.758V6.857h-2.758v12.857zm12.674-13.191c-1.736 0-3.115.643-3.957 1.98V6.857h-2.758v12.857h2.758v-6.891c0-2.623 1.43-3.703 3.242-3.703 1.737 0 2.86 1.029 2.86 2.983v7.611h2.757V11.82c0-3.343-2.042-5.297-4.902-5.297zm17.982-4.809v6.969c-.971-1.337-2.477-2.16-4.468-2.16-3.473 0-6.358 2.931-6.358 6.763 0 3.805 2.885 6.763 6.358 6.763 1.991 0 3.497-.823 4.468-2.186v1.851h2.757v-18h-2.757zM127.532 17.4c-2.298 0-4.034-1.723-4.034-4.114 0-2.392 1.736-4.115 4.034-4.115 2.297 0 4.034 1.723 4.034 4.115 0 2.391-1.737 4.114-4.034 4.114z"
             fill="currentColor"
