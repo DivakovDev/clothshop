e2aa2d73d92e6808ad5de406f134dc642ddfe714
Lyusi Divakov <divakovwork@gmail.com>
2023-11-17 09:05:35 +0200

    login page design

---
+++ app/login/page.tsx
@@ -0,0 +1,63 @@
+export default function Login() {
+  return (
+    <div>
+      <form className="gird sm:w-1/2 lg:w-2/6 mx-auto py-10 px-10 border-2 border-indigo-600 shadow-lg rounded-lg mt-2 sm:mt-4 md:mt-6 lg:mt-10">
+        <h3 className="text-center pt-4 pb-10 text-2xl font-bold text-indigo-600">Login into your account</h3>
+        <div className="mb-6">
+          <label
+            htmlFor="email"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            Your email
+          </label>
+          <input
+            type="email"
+            id="email"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            placeholder="emilygerber@gmail.com"
+            required
+          />
+        </div>
+        <div className="">
+          <label
+            htmlFor="password"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            Your password
+          </label>
+          <input
+            type="password"
+            id="password"
+            placeholder="•••••••••••"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            required
+          />
+        </div>
+        <p className="text-xs mb-6 ">Already don&apos;t have an account? <span> <a className="underline hover:text-indigo-600 font-semibold" href="">Sign Up</a></span></p>
+        <div className="flex items-start mb-6">
+          <div className="flex items-center h-5">
+            <input
+              id="remember"
+              type="checkbox"
+              defaultValue=""
+              className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+              required
+            />
+          </div>
+          <label
+            htmlFor="remember"
+            className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+          >
+            Remember me
+          </label>
+        </div>
+        <button
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
