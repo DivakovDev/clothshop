e422520ed230a40046c1ae59c7e6aa38bc79cb72
Lyusi Divakov <divakovwork@gmail.com>
2023-11-26 22:34:44 +0200

    make image selector and slider to work with butns

--- app/product/[handle]/page.tsx
+++ app/product/[handle]/page.tsx
@@ -1,13 +1,31 @@
-'use client';
-import React, { useState, useEffect } from 'react';
+"use client";
+import React, { useState, useEffect } from "react";
 import VariantSelector from "@/components/VariantSelector";
 import Image from "next/image";
 
-import { ProductData } from '@/types';
+import { ProductData } from "@/types";
 
 export default function Product({ params }: any) {
   const [productData, setProductData] = useState<ProductData | null>(null); // State for storing product data
-  const [showAllImages, setShowAllImages] = useState(false); // State for controlling image display
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const goLeft = () => {
+    setCurrentIndex((prevIndex) => 
+    (prevIndex > 0 ? prevIndex - 1 : 0)
+    );
+  };
+
+  // Function to handle right arrow click
+  const goRight = () => {
+    if (!productData || productData.product.images.length === 0) {
+      // Handle the case when productData is null or the images array is empty
+      return;
+    }
+  
+    setCurrentIndex((prevIndex) =>
+      prevIndex < productData.product.images.length - 4 ? prevIndex + 1 : 0
+    );
+  };
 
   useEffect(() => {
     const fetchProductData = async () => {
@@ -21,13 +39,14 @@ export default function Product({ params }: any) {
     fetchProductData();
   }, [params.handle]); // Dependency array, ensures the effect runs only when params.handle changes
 
-  const toggleShowAllImages = () => {
-    setShowAllImages(!showAllImages); // Toggle the state to show/hide images
-  };
-
   if (!productData) {
     return <div>Loading...</div>; // Display a loading message or spinner while data is being fetched
   }
+
+  const displayedImages = productData.product.images.slice(
+    currentIndex,
+    currentIndex + 4
+  );
 
   return (
     <div className="bg-wite">
@@ -41,61 +60,45 @@ export default function Product({ params }: any) {
           </div>
           <VariantSelector product={productData} />
         </div>
-  <div className="flex flex-col-reverse">
-  {/* Image selector */}
-  <div className="hidden mt-6 w-full max-w-2xl mx-auto sm:block lg:max-w-none">
-    <div
-      className="grid grid-cols-4 gap-6"
-      aria-orientation="horizontal"
-      role="tablist"
-    >
-      <button
-        id="tabs-1-tab-1"
-        className="relative h-24 bg-white rounded-md flex items-center justify-center text-sm font-medium uppercase text-gray-900 cursor-pointer hover:bg-gray-50 focus:outline-none focus:ring focus:ring-offset-4 focus:ring-opacity-50"
-        aria-controls="tabs-1-panel-1"
-        role="tab"
-        type="button"
-      >
-        <span className="sr-only"> Angled view </span>
-        <span className="absolute inset-0 rounded-md overflow-hidden">
-          <Image
-            src="https://tailwindui.com/img/ecommerce-images/product-page-03-product-01.jpg"
-            alt=""
-            className="w-full h-full object-center object-cover"
-            width={640}
-            height={537}
-          />
-        </span>
-        {/* Selected: "ring-indigo-500", Not Selected: "ring-transparent" */}
-        <span
-          className="ring-transparent absolute inset-0 rounded-md ring-2 ring-offset-2 pointer-events-none"
-          aria-hidden="true"
-        />
-      </button>
-      {/* More images... */}
-    </div>
-  </div>
-  <div className="w-full aspect-w-1 aspect-h-1">
-    {/* Tab panel, show/hide based on tab state. */}
-    <div
-      id="tabs-1-panel-1"
-      aria-labelledby="tabs-1-tab-1"
-      role="tabpanel"
-      tabIndex={0}
-    >
-      <Image
-        src="https://tailwindui.com/img/ecommerce-images/product-page-03-product-01.jpg"
-        alt="Angled front view with bag zipped and handles upright."
-        className="w-full h-full object-center object-cover sm:rounded-lg"
-        width={640}
-            height={537}
-      />
-    </div>
-    {/* More images... */}
-  </div>
+        <div className="flex flex-col">
+          {/* Main Image Section */}
+          <div className="w-full aspect-w-1 aspect-h-1">
+            <Image
+              src={productData.product.images[0].src}
+              alt="Selected Product Image"
+              className="w-full h-full object-center object-cover sm:rounded-lg"
+              width={640}
+              height={537}
+            />
+          </div>
 
+           {/* Image selector */}
+           <div className="flex flex-row w-full h-28 justify-center items-center mt-10">
+          <button className="p-2 w-16 h-full bg-indigo-600 text-white font-bold border rounded-l-lg" onClick={goLeft}>left</button>
+          <div className="w-full h-full max-w-2xl mx-auto sm:block lg:max-w-none">
+            
+            <div className="grid grid-cols-4 grid-rows-1">
+              
+                {displayedImages.map((image) =>   (
+                  <div className="h-28 border-2 border-gray-300" key={image.id}>
+                    <Image
+                      src={image.src}
+                      alt="Selected product variation of Images"
+                      className="w-full h-full object-center object-cover"
+                      width={640}
+                      height={537}
+                    />
+                  </div>
+                ))}
+              </div>
+  
+            </div>
+            <button className="p-2 w-16 h-full bg-indigo-600 text-white font-bold rounded-r-lg" onClick={goRight}>right</button>
+
+          </div>
+          
+        </div>
       </div>
-    </div>
     </div>
   );
 }
